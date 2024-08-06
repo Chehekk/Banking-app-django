d39d2e3c3451c5da8a78f4e8c6c188903330b0c6
Chehekk <chehek.khindria@gmail.com>
2024-08-06 19:43:49 +0530

    ave the changes to git

--- banking_system/urls.py
+++ banking_system/urls.py
@@ -23,9 +23,9 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
     path(
         'transactions/',
-        include('transactions.urls', namespace='transactions')
+        include('transactions.urls', namespace='transactions'),
     )
 ]
