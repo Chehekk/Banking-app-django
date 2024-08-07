4d899c2d47476ac8a11b805ed60fa2f7bf606735
Chehekk <chehek.khindria@gmail.com>
2024-08-07 18:35:21 +0530

    save changes to github

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,37 +2,23 @@ from django import forms
 from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
-
 from .models import User, BankAccountType, UserBankAccount, UserAddress
 from .constants import GENDER_CHOICE
 
-
 class UserAddressForm(forms.ModelForm):
-
     class Meta:
         model = UserAddress
-        fields = [
-            'street_address',
-            'city',
-            'postal_code',
-            'country'
-        ]
+        fields = ['street_address', 'city', 'postal_code', 'country']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         for field in self.fields:
             self.fields[field].widget.attrs.update({
-                'class': (
-                    'appearance-none block w-full bg-gray-200 '
-                    'text-gray-700 border border-gray-200 rounded '
-                    'py-3 px-4 leading-tight focus:outline-none '
-                    'focus:bg-white focus:border-gray-500'
-                )
+                'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
             })
 
-
 class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
     account_type = forms.ModelChoiceField(
         queryset=BankAccountType.objects.all(),
         required=True,
@@ -42,55 +28,30 @@ class UserRegistrationForm(UserCreationForm):
     birth_date = forms.DateField()
 
     class Meta:
-        model = UserBankAccount
-        fields = [
-            'first_name',
-            'last_name',
-            'email',
-            'account_type',
-            'account_number',
-            'password1',
-            'password2',
-        ]
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         for field in self.fields:
             self.fields[field].widget.attrs.update({
-                'class': (
-                    'appearance-none block w-full bg-gray-200 '
-                    'text-gray-700 border border-gray-200 '
-                    'rounded py-3 px-4 leading-tight '
-                    'focus:outline-none focus:bg-white '
-                    'focus:border-gray-500'
-                )
+                'class': 'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
             })
 
     @transaction.atomic
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user.email = self.cleaned_data.get('email')
         if commit:
             user.save()
-            account_type = self.cleaned_data.get('account_type')
-            gender = self.cleaned_data.get('gender')
-            birth_date = self.cleaned_data.get('birth_date')
-
             UserBankAccount.objects.create(
                 user=user,
-                gender=gender,
-                birth_date=birth_date,
-                account_type=account_type,
-                account_no=(
-                    user.id +
-                    settings.ACCOUNT_NUMBER_START_FROM
-                )
+                account_type=self.cleaned_data.get('account_type'),
+                gender=self.cleaned_data.get('gender'),
+                birth_date=self.cleaned_data.get('birth_date'),
+                account_number=user.id + settings.ACCOUNT_NUMBER_START_FROM
             )
         return user
-    
-    from django import forms
-from .models import UserBankAccount, BankAccountType
 
 class UserBankAccountForm(forms.ModelForm):
     account_type = forms.ModelChoiceField(
@@ -102,4 +63,3 @@ class UserBankAccountForm(forms.ModelForm):
     class Meta:
         model = UserBankAccount
         fields = ['account_type', 'account_number', 'balance', 'gender', 'birth_date']
-
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
-
 from django.contrib.auth.models import AbstractUser
+
 from django.core.validators import (
     MinValueValidator,
     MaxValueValidator,
