f0c8bee88b4366c185a9d9dc88c20b11805be9c5
Chehekk <chehek.khindria@gmail.com>
2024-08-08 21:01:49 +0530

    commit to github

--- accounts/views.py
+++ accounts/views.py
@@ -1,57 +1,47 @@
 from django.contrib import messages
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, login, logout
 from django.contrib.auth.views import LoginView
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, RedirectView
 
-from .forms import UserRegistrationForm, UserAddressForm,UserBankAccountForm
-
-def index(request):
-    return render(request, 'index.html')
+from .forms import UserRegistrationForm, UserAddressForm, UserBankAccountForm
 
 User = get_user_model()
 
-
 class UserRegistrationView(TemplateView):
-    model = User
-    form_class = UserRegistrationForm
     template_name = 'accounts/user_registration.html'
 
     def dispatch(self, request, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return HttpResponseRedirect(
-                reverse_lazy('transactions:transaction_report')
-            )
+            return HttpResponseRedirect(reverse_lazy('transactions:transaction_report'))
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         registration_form = UserRegistrationForm(self.request.POST)
         address_form = UserAddressForm(self.request.POST)
+        bank_account_form = UserBankAccountForm(self.request.POST)
 
-        if registration_form.is_valid() and address_form.is_valid():
+        if registration_form.is_valid() and address_form.is_valid() and bank_account_form.is_valid():
             user = registration_form.save()
             address = address_form.save(commit=False)
             address.user = user
             address.save()
 
+            bank_account = bank_account_form.save(commit=False)
+            bank_account.user = user
+            bank_account.save()
+
             login(self.request, user)
-            messages.success(
-                self.request,
-                (
-                    f'Thank You For Creating A Bank Account. '
-                    f'Your Account Number is {user.account.account_no}. '
-                )
-            )
-            return HttpResponseRedirect(
-                reverse_lazy('transactions:deposit_money')
-            )
+            messages.success(self.request, 'Thank you for creating a bank account!')
+            return HttpResponseRedirect(reverse_lazy('transactions:deposit_money'))
 
         return self.render_to_response(
             self.get_context_data(
                 registration_form=registration_form,
-                address_form=address_form
+                address_form=address_form,
+                bank_account_form=bank_account_form
             )
         )
 
@@ -60,14 +50,14 @@ class UserRegistrationView(TemplateView):
             kwargs['registration_form'] = UserRegistrationForm()
         if 'address_form' not in kwargs:
             kwargs['address_form'] = UserAddressForm()
+        if 'bank_account_form' not in kwargs:
+            kwargs['bank_account_form'] = UserBankAccountForm()
 
         return super().get_context_data(**kwargs)
 
-
 class UserLoginView(LoginView):
-    template_name='accounts/user_login.html'
+    template_name = 'accounts/user_login.html'
     redirect_authenticated_user = True
-
 
 class LogoutView(RedirectView):
     pattern_name = 'home'
@@ -76,37 +66,3 @@ class LogoutView(RedirectView):
         if self.request.user.is_authenticated:
             logout(self.request)
         return super().get_redirect_url(*args, **kwargs)
-    
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')  # or wherever you want to redirect
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'register.html', {'form': form})
-
-from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm, UserBankAccountForm
-
-def register(request):
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        account_form = UserBankAccountForm(request.POST)
-        if user_form.is_valid() and account_form.is_valid():
-            user = user_form.save()
-            bank_account = account_form.save(commit=False)
-            bank_account.user = user
-            bank_account.save()
-            return redirect('some-success-url')
-    else:
-        user_form = UserRegistrationForm()
-        account_form = UserBankAccountForm()
-
-    return render(request, 'registration/register.html', {
-        'user_form': user_form,
-        'account_form': account_form,
-    })
-
-
